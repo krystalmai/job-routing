40c72ffce8951c7a72c463d95abb1149561fc53d
krystalmai <83529994+krystalmai@users.noreply.github.com>
2022-07-01 23:48:50 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "./App.css";
 import Paper from "@mui/material/Paper";
 // import { grey, deepOrange, amber } from '@mui/material/colors';
 import SearchAppBar from "./components/SearchAppBar";
